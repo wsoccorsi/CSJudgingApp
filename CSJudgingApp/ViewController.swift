96c52dae6030c6bc1bc50f1a0942559599e7c906
williamsoccorsi <wsoccors@uvm.edu>
2019-03-17 16:18:45 -0400

    Initial commit

---
+++ CSJudgingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSJudgingApp
+//
+//  Created by William  Soccorsi on 3/17/19.
+//  Copyright © 2019 William  Soccorsi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
